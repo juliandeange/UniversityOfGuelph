38713d8daac30cd0f00d72eb063abf5b75f0206b
juliandeange <julian.deangelis@gmail.com>
2017-12-17 13:29:37 -0500

    Gen Perm and Binary Strings

---
+++ CIS2910/BinaryStrings/BinaryStrings/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BinaryStrings {
+    class MainClass {
+        public static void Main(string[] args) {
+
+            int subString = 0;
+            int zeroCount = 0;
+            int totalZeroCount = 0;
+            List<string> binaryStrings = new List<string>();
+
+            Console.WriteLine("Enter binary string length N > 5");
+            int start = Convert.ToInt32(Console.ReadLine()) - 1;
+            int n = start + 1;
+            double e = Math.Pow(2, start + 1);
+            int end = Convert.ToInt32(e) - 1;
+
+            //Converts integer to binary of length N 
+            //Adds binary string to list
+            for (int i = 0; i <= end; i++) {
+                string temp = Convert.ToString(i, 2);
+
+                while (temp.Length < n) {
+                    temp = "0" + temp;
+                }
+                binaryStrings.Add(temp);
+            }
+            Console.WriteLine("-----");
+            foreach (var str in binaryStrings) {
+                Console.WriteLine(str);
+                if (str.Contains("1010"))
+                    subString++;
+
+                for (int p = 0; p < str.Length; p++)
+                    if (str[p] == '0')
+                        zeroCount++;
+                if (zeroCount >= 4)
+                    totalZeroCount++;
+                zeroCount = 0;
+            }
+            Console.WriteLine("-----");
+            Console.WriteLine("Strings with 1010 = " + subString);
+            Console.WriteLine("Strings with at least four 0s = " + totalZeroCount);
+        }
+    }
+}
+
---
+++ CIS2910/GeneratePermutations/GeneratePermutations/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GeneratePermutations {
+    class MainClass {
+        public static void Main(string[] args) {
+
+            int num = Convert.ToInt32(Console.ReadLine());
+            int totalPermutations = 0;
+            List<int> n = new List<int>(); //stores the string 1,2, ..., n
+            List<int> nsub = new List<int>(); //stores numbers before decrease in sequence
+            List<int> original = new List<int>(); //copy of original sequence
+            int isdis = 1;
+            int derangement = 0;
+
+            foreach (var v in n) Console.Write(v);
+            for (int i = 1; i <= num; i++) {
+                n.Add(i);
+                original.Add(i);
+            }
+            int factorial = 1;
+            for (int i = num; i > 0; i--)  //calculates the factorial of permutations
+                factorial = factorial * i;
+            foreach (var v in n) Console.Write(v);
+            for (int y = 0; y < factorial; y++) {
+                n.Reverse();
+                Console.WriteLine();
+                for (int i = 0; i < n.Count - 1; i++) { //Looks for decrease in sequence
+                    nsub.Add(n[i]);
+                    if (n[i] > n[i + 1]) {
+                        int toSwap = n[i + 1];
+                        int toReplace = nsub.Where(l => l > n[i + 1]).Min();
+                        n[i + 1] = toReplace;  //replaces decreased value with largest min number
+                        nsub.Remove(toReplace);
+                        nsub.Add(toSwap);
+                        nsub = nsub.OrderByDescending(p => p).ToList();
+
+                        //******SUB IN NSUB OT BEGINNING OF N*****
+                        for (int p = 0; p < nsub.Count(); p++)  //re-orders lowest to highest
+                            n[p] = nsub[p];
+                        n.Reverse();
+                        nsub.Clear();
+
+                        for (int o = 0; o < n.Count; o++){ //checks for disrangements by seeing if every value..
+                            if (original[o] != n[o]) {     //   ..is not the original 
+                                isdis = 1;
+                                continue;
+                            }
+                            if (original[o] == n[o]) {
+                                isdis =  0;
+                                break;
+                            } 
+                        }
+                        if (isdis == 1) {
+                            derangement++; //counts disrangements
+                            Console.Write("*");
+                        }
+                        isdis = 0;
+                        foreach (var v in n) Console.Write(v);
+                        break;
+                    }//end if
+                }//end inner (for) looking through letters
+                totalPermutations++;
+            }//end (for) factorial counter
+            Console.WriteLine("total perms = " + totalPermutations);
+            Console.WriteLine("disarrangements = " + derangement);
+            double prob = Convert.ToDouble(derangement) / Convert.ToDouble(totalPermutations);
+            Console.WriteLine("probability of derangement = " + prob);
+        }//end main
+    }//end class
+}//end namespace
